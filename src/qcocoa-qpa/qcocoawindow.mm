f3707f1516f1393235c178d5829cf3f88d5844e1
R.J.V. Bertin <rjvbertin@gmail.com>
2017-04-06 19:03:23 +0200

    01-patch-missing-autoreleasepools.diff
    Add autorelease pools to functions that (used to) need them. This is safe
    and comes at almost no cost.
    
    Committed from host : Portia.local

--- src/qcocoa-qpa/qcocoawindow.mm
+++ src/qcocoa-qpa/qcocoawindow.mm
@@ -1727,6 +1727,8 @@ void QCocoaWindow::applyContentBorderThickness(NSWindow *window)
     if (!window)
         return;
 
+    QMacAutoReleasePool pool;
+
     if (!m_drawContentBorderGradient) {
         [window setStyleMask:[window styleMask] & ~NSTexturedBackgroundWindowMask];
         [[[window contentView] superview] setNeedsDisplay:YES];
